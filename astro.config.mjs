811059a74cb6b20707c03db74cdfdc7745957a3b
George Huebner <george@feyor.sh>
2024-09-20 06:50:57 -0500

    ramshackle version up
    Cobbled together from [Astro Ink](https://github.com/one-aalam/astro-ink)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+    site: 'https://sigquantum.com',
+    output: 'static',
+    // output: 'server', SSR
+    // adapter: cloudflare({
+    //     imageService: 'cloudflare'
+    // }),
     vite: {
         server: {
+            ssr: {
+                noExternal: false,
+            },
             fs: {
                 // Allow serving files from one level up to the project root
-                allow: ['/nix/store/'],
+                allow: ['/nix/store/', './src/styles/'],
             },
         },
     },
-
     integrations: [tailwind()],
 })
---
+++ src/config.ts
@@ -0,0 +1,31 @@
+import type { NavItems } from "./types";
+
+export const NAV_ITEMS: NavItems = {
+	home: {
+		path: "/",
+		title: "home",
+	},
+	meetings: {
+		path: "/meetings",
+		title: "meetings",
+	},
+	fall_fest: {
+		path: "/fall-fest",
+		title: "fall fest",
+	},
+	resources: {
+		path: "/resources",
+		title: "resources",
+	},
+	// about: {
+	// 	path: "/about",
+	// 	title: "about",
+	// },
+};
+
+export const SITE = {
+	name: "SIGQuantum",
+	title: "SIGQuantum",
+	description: "ACM@UIUC's Special Interest Group for Quantum Information",
+    url: "https://sigquantum.com",
+};
