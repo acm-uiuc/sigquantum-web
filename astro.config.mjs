6fbe1a7c43768b99c6da6ea2cbf47b8b35561e76
George Huebner <george@feyor.sh>
2024-09-20 00:21:42 -0500

    init tailwind, daisyui
    web dev yippee

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+    vite: {
+        server: {
+            fs: {
+                // Allow serving files from one level up to the project root
+                allow: ['/nix/store/'],
+            },
+        },
+    },
+
+    integrations: [tailwind()],
+})
