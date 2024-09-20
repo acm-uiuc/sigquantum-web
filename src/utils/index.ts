40ba2083dd5cd08c7b08b0e34a4b1049171297aa
George Huebner <george@feyor.sh>
2024-09-20 13:33:50 -0500

    good enough for now
    - <body> padding
    - better 404
    - redirect from index because it doesn't exist (yet)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,6 +14,26 @@ const MONTHS = [
 	"Dec",
 ];
 
+const XKCD = [
+  [1240, "Quantum Mechanics"],
+  [2200, "Unreachable State"],
+  [1591, "Bell's Theorem"],
+  [872, "Fairy Tales"],
+  [2028, "Complex Numbers"],
+  [435, "Purity"],
+  [538, "Security"],
+  [177, "Alice and Bob"]
+];
+
+export const randomXkcd = () => {
+  const xkcd = XKCD[Math.floor(Math.random() * XKCD.length)];
+  return {
+    "src": "https://imgs.xkcd.com/comics/" + xkcd[1].toLowerCase().split(' ').join('_').replace("'", '') + ".png",
+    "href": "https://xkcd.com/" + xkcd[0] + '/',
+    "name": xkcd[1],
+  };
+}
+
 export const toTitleCase = (str: string) =>
 	str.replace(/\w\S*/g, function (txt) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
